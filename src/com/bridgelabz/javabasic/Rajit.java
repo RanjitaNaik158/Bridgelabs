9ee8923cf5b6efa62ee44846270abb4147fc388b
Ranjitanaik <ranjitamnaik1997@gmail.com>
2022-06-20 21:34:32 +0530

    first commit

---
+++ src/com/bridgelabz/javabasic/Rajit.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.javabasic;
+
+public class Rajit 
+{
+
+	  int marks;
+	  String name;
+	  String place;
+
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Hi Rajith");
+	System.out.println("huuu");
+	    System.out.println("oot");
+	    }
+	    }
+
